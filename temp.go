4d6c2cdaef915aa3b537d913edfca8099768d6a4
Nayum Teixeira <68863624+Nayumt99@users.noreply.github.com>
2023-02-16 13:23:02 -0300

    atualização sobre operadores curtos

--- temp.go
+++ temp.go
@@ -9,9 +9,9 @@ const ebulicaoF float64 = 212.0
 
 //função principal
 func main() {
-
-	var tempF float64 = ebulicaoF            //variável do VALOR da temperatura em graus F
-	var tempC float64 = (tempF - 32) * 5 / 9 //conversão de F para C
+	//usando := esse operador curto, pode ser utilizado dentro de um codeblock.
+	tempF := ebulicaoF            //variável do VALOR da temperatura em graus F
+	tempC := (tempF - 32) * 5 / 9 //conversão de F para C
 	//apareça na tela o resultado
 	fmt.Println("A temperatura de ebulição da água em graus °F é", tempF)
 	fmt.Println("A temperatura de ebilução da água em graus °C é", tempC)
